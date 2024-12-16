7ed07cee9d362aed70752c51189da1d1fd8e0462
smart-developer <okovtn747@gmail.com>
2024-12-17 01:56:29 +0900

    add: faq section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import VideoPlayer from "@components/Home/VideoSection";
 import HomeHeader from "@components/Home/Home";
 import { Sections } from "@components/Home/Section";
 import CardSection from "@components/Home/CardSection";
-
+import FaqSection from "@components/Home/FaqSection";
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
       <Sections />
 
       <CardSection />
+      <FaqSection />
     </div>
   );
 };
