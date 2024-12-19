40a79d02de127bd23d57e72d3af9968f6b691a69
Webet dev <153311377+Igamingdev1@users.noreply.github.com>
2024-12-19 07:19:01 +0100

    review: styles on pages

--- src/components/Home/FaqCard.tsx
+++ src/components/Home/FaqCard.tsx
@@ -3,70 +3,67 @@ import Image from "next/image";
 import { MdArrowBackIos, MdKeyboardArrowUp } from "react-icons/md";
 
 interface Prop {
-  children: ReactElement | string;
-  title: string;
-  description: string;
-  icon: string;
-  isExpended?: boolean;
-  onToggle?: () => void;
+    children: ReactElement | string;
+    title: string;
+    description: string;
+    icon: string;
+    isExpended?: boolean;
+    onToggle?: () => void;
 }
 
 const FaqCard = ({
-  children,
-  title,
-  description,
-  icon,
-  onToggle = () => {},
-  isExpended = false,
+    children,
+    title,
+    description,
+    icon,
+    onToggle = () => { },
+    isExpended = false,
 }: Prop) => {
-  return (
-    <div className="flex flex-col ">
-      <div
-        onClick={onToggle}
-        className={`flex xs:px-10   xs:py-8 p-4 rounded-2xl xs:rounded-3xl       relative border-[1px] 
-          animated-element gap-8 ${
-            isExpended
-              ? "bg-[#1755F4] hover:bg-[#4772df] border-transparent"
-              : "bg-[#3D424D80]  hover:bg-gray-800 border-gray-600"
-          }`}
-      >
-        <Image
-          src={icon}
-          draggable="false"
-          alt=""
-          width={100}
-          height={100}
-          className="w-14 h-14 aspect-square rounded-full hidden sm:block"
-        />
-        <div>
-          <h4 className={`xs:text-2xl  text-xl font-semibold text-white`}>
-            {title}
-          </h4>
-          <div className=" text-gray-300">{description}</div>
-        </div>
-        <div className="w-4"></div>
-        <MdKeyboardArrowUp
-          className={` transition duration-150 absolute  right-2 xs:right-8 
-            top-1/2 -translate-y-1/2 w-10 h-10 text-gray-300  ${
-              isExpended ? "rotate-0" : "rotate-180"
-            } `}
-        />
-      </div>
-      <div
-        className={`bg-[#3D424D80]  transition duration-150  border-gray-600  rounded-2xl overflow-hidden 
+    return (
+        <div className="flex flex-col ">
+            <div
+                onClick={onToggle}
+                className={`flex xs:px-10   xs:py-8 p-4 rounded-2xl xs:rounded-3xl       relative border-[1px] 
+          animated-element gap-8 ${isExpended
+                        ? "bg-[#1755F4] hover:bg-[#4772df] border-transparent"
+                        : "bg-[#3D424D80]  hover:bg-gray-800 border-gray-600"
+                    }`}
+            >
+                <Image
+                    src={icon}
+                    draggable="false"
+                    alt=""
+                    width={100}
+                    height={100}
+                    className="w-14 h-14 aspect-square rounded-full hidden sm:block"
+                />
+                <div>
+                    <h4 className={`xs:text-2xl  text-xl font-semibold text-white`}>
+                        {title}
+                    </h4>
+                    <div className=" text-gray-300">{description}</div>
+                </div>
+                <div className="w-4"></div>
+                <MdKeyboardArrowUp
+                    className={` transition duration-150 absolute  right-2 xs:right-8 
+            top-1/2 -translate-y-1/2 w-10 h-10 text-gray-300  ${isExpended ? "rotate-0" : "rotate-180"
+                        } `}
+                />
+            </div>
+            <div
+                className={`bg-[#3D424D80]  transition duration-150  border-gray-600  rounded-2xl overflow-hidden 
             xs:rounded-3xl
            whitespace-pre-line
            xs:px-10
-          animated-element  ${
-            isExpended
-              ? "    xs:py-8 p-4   mt-4"
-              : " h-0 py-0 xs:py-0  mt-0 rounded-none"
-          }`}
-      >
-        {children}
-      </div>
-    </div>
-  );
+          animated-element  ${isExpended
+                        ? "    xs:py-8 p-4   mt-4"
+                        : " h-0 py-0 xs:py-0  mt-0 rounded-none"
+                    }`}
+            >
+                {children}
+            </div>
+        </div>
+    );
 };
 
 export default FaqCard;
--- src/components/Stake/modal.tsx
+++ src/components/Stake/modal.tsx
@@ -3,40 +3,40 @@ import { motion, AnimatePresence } from "framer-motion";
 import { IoMdClose } from "react-icons/io";
 
 const modalVariant = {
-  initial: { opacity: 0 },
-  isOpen: { opacity: 1 },
-  exit: { opacity: 0 },
+    initial: { opacity: 0 },
+    isOpen: { opacity: 1 },
+    exit: { opacity: 0 },
 };
 const containerVariant = {
-  initial: { top: "-50%", transition: { type: "spring" } },
-  isOpen: { top: "50%" },
-  exit: { top: "-50%" },
+    initial: { top: "-50%", transition: { type: "spring" } },
+    isOpen: { top: "50%" },
+    exit: { top: "-50%" },
 };
 export const Modal = ({ handleClose, children, isOpen }: any) => {
-  return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          className=" backdrop-blur-sm fixed top-0 left-0 z-[100000] w-full h-full bg-[#0000007f]"
-          initial={"initial"}
-          animate={"isOpen"}
-          exit={"exit"}
-          variants={modalVariant}
-          onClick={handleClose}
-        >
-          <motion.div
-            className="w-full  max-w-[440px] h-full max-h-[400px] border-[1px] border-white  bg-[#ffffff36] absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl p-6 "
-            variants={containerVariant}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <IoMdClose
-              className=" text-3xl absolute right-6 top-6"
-              onClick={handleClose}
-            />
-            {children}
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    className=" backdrop-blur-sm fixed top-0 left-0 z-[100000] w-full h-full bg-[#0000007f]"
+                    initial={"initial"}
+                    animate={"isOpen"}
+                    exit={"exit"}
+                    variants={modalVariant}
+                    onClick={handleClose}
+                >
+                    <motion.div
+                        className="w-full  max-w-[440px] h-full max-h-[400px] border-[1px] border-white  bg-[#ffffff36] absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl p-6 "
+                        variants={containerVariant}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <IoMdClose
+                            className=" text-3xl absolute right-6 top-6"
+                            onClick={handleClose}
+                        />
+                        {children}
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 };
