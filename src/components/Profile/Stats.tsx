3c543c9377f969553165b9475494a63b1db55182
smart-developer <okovtn747@gmail.com>
2025-02-18 05:15:31 +0900

    feat: profile page, number format

--- src/components/Profile/Stats.tsx
+++ src/components/Profile/Stats.tsx
@@ -1,15 +1,15 @@
-import { formatNumber } from "@utils/index";
+import { CountUp } from "@components/ui/CountUp";
 
 export function Stats({ stakedAmount, totalZKIP }: { stakedAmount: number; totalZKIP: number; }) {
     return (
         <div className="grid grid-cols-2 gap-4 bg-gray-800 rounded-lg p-4 text-center sm:text-start">
             <div>
                 <h3 className="text-lg font-semibold text-gray-400">Staked ZK</h3>
-                <p className="text-2xl font-bold">{formatNumber(stakedAmount)}</p>
+                <p className="text-2xl font-bold"><CountUp end={stakedAmount} format="0,0.[000]" /></p>
             </div>
             <div>
                 <h3 className="text-lg font-semibold text-gray-400">Total ZKIP</h3>
-                <p className="text-2xl font-bold">{totalZKIP}</p>
+                <p className="text-2xl font-bold"><CountUp end={totalZKIP} format="0,0.[000]" /></p>
             </div>
         </div>
     );
